42c6a619ec5e48882f8d76548de07c56f4577b9a
Mohammed Hossam <mohamedhossamelsebaey23@gmail.com>
2026-02-26 08:08:12 +0200

    driver update

--- sensors_drivers/sensors_src/MPU_6050.c
+++ sensors_drivers/sensors_src/MPU_6050.c
@@ -197,8 +197,31 @@ void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *MPUcfg , double dt)
  *
  * /
 
+// another method
+/*
+ * void vSensingAndControlTask(void * pvParameters) {
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    TickType_t previous_time = xLastWakeTime;
+    const TickType_t xFrequency = pdMS_TO_TICKS(10); // الهدف: 10ms
 
+    for(;;) {
+        // 1. نام لحد ما الـ 10ms تخلص
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
 
+        // 2. احسب الـ dt الفعلي اللي عدى بجد (عشان لو الـ RTOS اتأخر ملي ثانية ولا حاجة)
+        TickType_t current_time = xTaskGetTickCount();
+        float actual_dt = (float)(current_time - previous_time) / 1000.0f;
+        previous_time = current_time;
+
+        // 3. مرر الـ dt الحقيقي والموحد لكل الروبوت
+        ACS758_Update(&myBattery, adc_buffer[0], actual_dt);
+        MPU6050_Read_And_Process(&hi2c1, &myMPUStruct, actual_dt);
+        Calculate_Odometry(&myOdomStruct, actual_dt);
+        Run_PID_Controllers(actual_dt);
+        Update_Global_Variables();
+    }
+}
+ * */
 
 
 
